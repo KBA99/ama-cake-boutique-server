3b4791de1f2c5143948353e5ca066cafb6bd678d
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-12-27 18:51:07 +0000

    feat: use tabs instead of spaces in email cakeEnquirySchema and and change required validation for fields

--- models/cakeEnquire.js
+++ models/cakeEnquire.js
@@ -1,50 +1,51 @@
-import mongoose from "mongoose"
+const mongoose = require("mongoose")
 
-const cakeEnquire = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true
-  },
-  fullName: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: mongoose.SchemaTypes.Email,
-    required: true
-  },
-  dateOfEvent: {
-    type: Date,
-    required: true
-  },
-  partyOccasion: {
-    type: String,
-    required: true
-  },
-  cakeTaste: {
-    type: String,
-    required: true
-  },
-  cakeTopper: {
-    type: Boolean,
-    required: true
-  },
-  extraDetails: {
-    type: Text,
-    required: false
-  },
-  /**
-   * Will need to add the functionality to be able to add an image and retrieve that image from the database
-   *image: {
-    type: ////,
-    required: false,
-  }
-   */
-  inputDate: {
-    type: Date,
-    required: true,
-    default: Date.now
-  }
+const cakeEnquireSchema = new mongoose.Schema({
+	// id: {
+	//   type: String,
+	//   required: false
+	// },
+	fullName: {
+		type: String,
+		required: false
+	},
+	email: {
+		type: String,
+		required: false,
+		lowercase: true
+	},
+	dateOfEvent: {
+		type: Date,
+		required: false
+	},
+	partyOccasion: {
+		type: String,
+		required: false
+	},
+	cakeTaste: {
+		type: String,
+		required: false
+	},
+	cakeTopper: {
+		type: Boolean,
+		required: false
+	},
+	extraDetails: {
+		type: String,
+		required: false
+	},
+	/**
+	 * Will need to add the functionality to be able to add an image and retrieve that image from the database
+	 *image: {
+		type: ////,
+		required: false,
+	}
+	*/
+	inputDate: {
+		type: Date,
+		required: true,
+		default: () => Date.now()
+	}
 })
 
-module.exports = mongoose.model('Enquire', cakeEnquire)
+module.exports = mongoose.model('Enquire', cakeEnquireSchema)
