b6180e090f51b5727a69c5f8688a3672367f9738
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-12-29 14:44:33 +0000

    feat: add functionality to add email to mailing list

--- controllers/emailRestController.js
+++ controllers/emailRestController.js
@@ -1,19 +1,28 @@
+const Email = require("../models/mailingList")
+
 const addToMailingList = async (req, res) => {
-	const {email} = req.params
-    
+//TODO Check the collection to see if it already has the email in the mailing list, if so then sends an error message to the user
+	const {email} = req.body
     try {
-        const mailingList = await Email.Create({
-            email
-        })
-        res.status(201).send(`${email} added to mailing list`)
-        console.log({mailingList})
+		if(!Email.exists({email})) {
+			const mailingList = await Email.create({
+				email
+			})
+			res.status(201).send(`${email} added to mailing list`)
+			console.log({mailingList})
+		} else {
+			res.status(403).send({error: "User is already registered to the mailing list"})
+		}
     } catch (error) {
         console.log(error.message)
     }
+}
 
-    res.status(201).send(`${email} added to mailing list`)
+const getEmails = async (req, res) => {
+	res.send("get mailing list")
 }
 
 module.exports = {
-	addToMailingList
+	addToMailingList,
+	getEmails
 }
