d5232540f6de43945af9862ea60ea1bd2047e361
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-12-29 14:50:13 +0000

    feat: add get email route to api

--- routes/api/email.js
+++ routes/api/email.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router()
 
-const { addToMailingList } = require ('../../controllers/emailRestcontroller.js')
+const { addToMailingList, getEmails } = require ('../../controllers/emailRestcontroller.js')
 
 router.post('/', addToMailingList);
+
+//TODO Make mongo query that returns all of the emails on the mailing list but to a protected route
+router.get('/', getEmails);
 
 module.exports = router;
